fa2e7920058ad69e31f5c2c94438a8fdea0d1720
Hadrien Devichi <h.devichi@wanadoo.fr>
2013-10-07 20:52:02 +0200

    add sudoky

---
+++ src/sudoku/Move.java
@@ -0,0 +1,45 @@
+package sudoku;
+
+public class Move {
+
+	private int x;
+	private int y;
+	private int value;
+	
+	public Move(int x, int y) {
+		this.x = x;
+		this.y = y;
+		value = -1;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		if (value < 0)
+			throw new RuntimeException();
+		if (value > 9)
+			throw new RuntimeException();
+		
+		this.value = value;
+	}
+	
+	public String toString() {
+		StringBuffer b = new StringBuffer();
+		b.append('(').append(x).append(',').append(y).append(")->").append(value);
+		return b.toString();
+	}
+	
+}
